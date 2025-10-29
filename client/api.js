a5ff3ef263e37b50b8130cccbb095c52f0c7ef1d
gauri Sonawane <gaurisonawane918@gmail.com>
2025-10-29 14:27:56 +0530

    fixed deployment issue

--- client/api.js
+++ client/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://storerater-4.onrender.com",
   headers: { "Content-Type": "application/json" },
 });
 
